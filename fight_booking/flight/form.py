a6a632266201d2fd2bf3b0d848813fe968c9c202
walter_pang <kinzi@live.hk>
2020-05-04 01:45:10 +0800

    book
    update book flight

---
+++ fight_booking/flight/form.py
@@ -0,0 +1,48 @@
+from flask_wtf import Form
+from wtforms import StringField, SubmitField, validators, TextAreaField, IntegerField, SelectField, DateTimeField, \
+    DateField
+from fight_booking.flight.model import Flight, Airport, airline
+from datetime import date
+
+
+class Form_Testbooking(Form):
+    """
+    建置blog表頭的表單
+    """
+    flight_id = StringField('flight_id', validators=[
+        validators.DataRequired(),
+        validators.Length(1, 30)
+    ])
+    flightNo = StringField('flightNo', validators=[
+        validators.Length(0, 300)
+    ])
+
+    seat_no = IntegerField('seatNo', validators=[
+         validators.NumberRange(1,250)])
+    submit = SubmitField('Create Blog')
+
+class From_search_flight(Form):
+
+    from_place = SelectField('From',coerce=str)
+    to_place = SelectField('To',coerce=str)
+
+    submit = SubmitField('Search Flight')
+
+
+    def __init__(self):
+        super(From_search_flight, self).__init__()
+        self.from_place.choices = self._get_airport_localtion()
+        self.to_place.choices = self._get_airport_localtion()
+
+    def _get_airport_localtion(self):
+        obj = Airport.query.with_entities( Airport.airportNname,Airport.airportNname).all()
+        return obj
+
+    def _get_airline(self):
+        obj = airline.query.all()
+        return obj
+
+class From_book_confirm(Form):
+    seat_no = IntegerField('seatNo', validators=[
+        validators.NumberRange(1, 250)])
+    submit = SubmitField('confirm')
---
+++ sql/test.sql
@@ -0,0 +1,10 @@
+select * from tbl_flight;
+select * from booking;
+
+drop database airline;
+create database airline;
+
+update tbl_user set user_confirm = 1 where user_id = 1;
+
+insert into booking values(null,1,1,145,NOW(),NOW(),8899);
+
