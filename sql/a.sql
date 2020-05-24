3e99f90bf2ea476f76850b6f11bbf3660624e03b
walter_pang <kinzi@live.hk>
2020-05-24 13:39:00 +0800

    db test data
    db test data

--- sql/a.sql
+++ sql/a.sql
@@ -29,15 +29,23 @@ insert into airplacnes values(null,"Vietnam Airlines", 777, 500);
 insert into airplacnes values(null,"American Airlines", 350 ,500);
 insert into airplacnes values(null,"Delta Airlines", 777, 250);  
 
-insert into tbl_flight (airlineName,airplacneModel,from_place,to_place,depart_at_from,arrival_at_to,available,collected,rate) 
-			  	 values("Emirates", 777, "JFK",  "O'Hare", "2017-06-19 22:30:00", "2017-06-20 06:00:00", "Upcoming", 777,999);
+insert into tbl_flight (airlineName,airplacneModel,from_place,to_place,depart_Date,depart_Time,available,price) 
+			  	 values("Emirates", 777, "JFK",  "O'Hare", "2020-06-19", "22:30:00", "Upcoming",999);
                  
-insert into tbl_flight (airlineName,airplacneModel,from_place,to_place,depart_at_from,arrival_at_to,available,collected , rate) 
-			  	 values("Delta Airlines", 777, "O'Hare", "JFK", "2017-04-03 20:45:00",  "2017-04-04 07:00:00", "Upcoming", 777,452);
+insert into tbl_flight (airlineName,airplacneModel,from_place,to_place,depart_Date,depart_Time,available,price)
+			  	 values("Delta Airlines", 777, "O'Hare", "JFK", "2020-04-03","20:45:00", "Upcoming",452);
                  
-insert into tbl_flight (airlineName,airplacneModel,from_place,to_place,depart_at_from,arrival_at_to,available,collected,rate) 
-			  	 values("Delta Airlines", 777, "O'Hare", "JFK", "2017-04-02 19:45:00",  "2017-04-02 23:00:00", "Upcoming", 777,2032);
-insert into tbl_flight (airlineName,airplacneModel,from_place,to_place,depart_at_from,arrival_at_to,available,collected,rate)   
-				 values("Delta Airlines", 777, "JFK", "DFW", "2017-04-19 22:30:00",  "2017-04-20 06:00:00", "Upcoming", 777,1523);
-                 
-                 
+insert into tbl_flight (airlineName,airplacneModel,from_place,to_place,depart_Date,depart_Time,available,price)
+			  	 values("Delta Airlines", 777, "O'Hare", "JFK", "2020-04-02","19:45:00", "Upcoming",2032);
+insert into tbl_flight (airlineName,airplacneModel,from_place,to_place,depart_Date,depart_Time,available,price)values
+					   ("Delta Airlines", 777, "JFK", "DFW", "2020-04-19","22:30:00","Upcoming",1523),
+					   ("Delta Airlines", 777, "O'Hare",  "JFK", "2020-04-02","19:45:00","Upcoming", 799),
+					   ("Korea Air", 797, "Incheon",  "JFK", "2020-04-30","19:45:00", "Upcoming", 2990),
+					   ("Korea Air", 777, "Incheon",  "JFK", "2020-04-30","3:00:00", "Notavailable", 1990),
+					   ("Korea Air", 777, "Incheon",  "JFK", "2020-04-30","7:00:00","Upcoming", 4990), 
+					   ("Vietnam Airlines", 777, "Tan Son Nhat",  "JFK", "2020-04-30","19:45:00",  "Upcoming", 1099),
+					   ("All Nippon Airways", 777, "Narita",  "O'Hare", "2020-05-19","19:45:00",  "Upcoming", 2990),
+					   ("Delta Airlines", 777, "JFK",   "DFW","2020-04-19","22:30:00", "Upcoming", 800),
+					   ("Lufthansa", 777, "JFK",  "DFW",  "2020-04-19","22:30:00", "Upcoming", 800), 
+					   ("American Airlines", 797, "JFK",   "DFW", "2020-04-19","22:30:00","Upcoming", 800);
+
